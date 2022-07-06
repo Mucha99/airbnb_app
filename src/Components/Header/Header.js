cefb1844f8a97025ed06f40b6d8da17dc20a07f9
Bartek Mucha <bartekmucha43@gmail.com>
2022-07-06 22:58:54 +0200

    Add styles for search box

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,8 +2,16 @@ import styles from './Header.module.scss';
 
 function Header() {
     return (
-        <div>
-            <h1 className={styles.title}>Header</h1>
+        <div className={styles.header}>
+            <div className={styles.box}>
+                <input type='checkbox' id={styles.check}/>
+                <div class={styles.search_box}>
+                    <input type='text' placeholder='Szukaj..' className={styles.search_input}/>
+                    <label for='check' className={styles.icon}>
+                        <i class='fas fa-search'></i>
+                    </label>
+                </div>  
+            </div>
         </div>
     );
 }
